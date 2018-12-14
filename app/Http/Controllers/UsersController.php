43156537ca2e1352971a338667f75f746b129ed3
dengao <2916012032@qq.com>
2018-12-14 20:22:29 +0800

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Dotenv\Validator;
 use Illuminate\Http\Request;
 use App\Models\User;
 
@@ -16,5 +17,27 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show',compact('user'));
+    }
+
+
+    //提交页面
+    public function store(Request $request)
+    {
+
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'email' =>'required|email|unique:users',
+            'password'=>'required|min:6|confirmed'
+        ]);
+
+        //返回创建的模型实例
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password)
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show',[$user]);
     }
 }
